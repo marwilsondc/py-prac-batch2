eff1aad1f3f033149c2b84da77c169e7d52152d5
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-04 23:45:06 +0800

    uploaded program 3

---
+++ 2prog03.py
@@ -0,0 +1,5 @@
+# Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
+first_number = int(input("minuend = "))
+second_number = int(input("subtrahend = "))
+
+print(first_number - second_number)
