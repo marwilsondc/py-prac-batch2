59236d08ab4e856cde977879945fbc7ddfb739f6
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-05 00:00:31 +0800

    uploaded program 5

---
+++ 2prog05.py
@@ -0,0 +1,5 @@
+# Prog05: Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
+first_number = int(input("first number = "))
+second_number = int(input("second number = "))
+
+print(first_number % second_number)
