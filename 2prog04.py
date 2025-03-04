946e74b5ece4d9595f573c47a1279990cd1dfb67
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-04 23:57:20 +0800

    uploaded program 4

---
+++ 2prog04.py
@@ -0,0 +1,6 @@
+# Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
+
+first_number = int(input("first number = "))
+second_number = int(input("second number = "))
+
+print(int(first_number / second_number))
