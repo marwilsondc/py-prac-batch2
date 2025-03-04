3092dbfd98e355629a5a4168a40583437acf4e9f
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-04 23:40:50 +0800

    uploaded program 2

---
+++ 2prog02.py
@@ -0,0 +1,6 @@
+# Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.\
+first_number = int(input("first number = "))
+second_number = int(input("second number ="))
+
+if first_number != second_number:
+    print("Not Equal")
