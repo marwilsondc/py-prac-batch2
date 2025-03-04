2d2a2c87aa6aadf158ab5fb465535085e09dcc8c
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-04 23:35:33 +0800

    uploaded program 1

---
+++ 2prog01.py
@@ -0,0 +1,9 @@
+# Prog01: Create a program that ask user to input 2 numbers. Print the smaller number.
+first_number = int(input("First number = "))
+second_number = int(input("Second number = "))
+
+if first_number < second_number:
+    print(first_number)
+
+else:
+    print(second_number)
